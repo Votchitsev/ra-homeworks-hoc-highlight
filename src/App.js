74a82a1009424a7f687a3ddaa8f6f3f0e3d8763e
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-11-04 18:03:07 +0200

    task complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { v4 } from 'uuid';
 
 function New(props) {
   return (
@@ -30,24 +31,58 @@ function Article(props) {
 function Video(props) {
   return (
     <div className="item item-video">
-      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" title='video' allowfullscreen></iframe>
+      <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" title='video' allowFullScreen></iframe>
       <p className="views">Просмотров: {props.views}</p>
     </div>
   )
 };
 
+function ContentItem(Component) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.views = props.views;
+    }
+
+    render() {
+      if (this.views >= 1000) {
+        return (
+          <Popular>
+            <Component {...this.props}/>
+          </Popular>
+        )
+      }
+
+      if (this.views < 100) {
+        return (
+          <New>
+            <Component {...this.props} />
+          </New>
+        )
+      }
+    }
+  }
+}
+
 function List(props) {
   return props.list.map(item => {
     switch (item.type) {
       case 'video':
+
+        const VideoWithHighlight = ContentItem(Video);
+
         return (
-            <Video {...item} />
+          <VideoWithHighlight {...item} key={ v4() } />
         );
 
       case 'article':
+
+        const ArticleWithHighlight = ContentItem(Article);
+
         return (
-            <Article {...item} />
+          <ArticleWithHighlight {...item} key={ v4() } />
         );
+
       default: 
         throw new Error('Unexpected type');
     }
@@ -55,7 +90,7 @@ function List(props) {
 };
 
 export default function App() {
-  const [list, setList] = useState([
+  const [list] = useState([
     {
       type: 'video',
       url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
